77c1e6380c5910cc622a11ec4d9426f16c1bc5f2
alexander-g <3867427+alexander-g@users.noreply.github.com>
2025-10-29 23:56:07 +0000

    compile to wasm

---
+++ main_wasm.cpp
@@ -0,0 +1,40 @@
+#include <Eigen/Dense>
+#include <Eigen/Core>
+#include <unsupported/Eigen/CXX11/Tensor>
+
+#include "./morphology.hpp"
+
+
+
+enum ERRORCODES {
+    OK = 0,
+    OUTPUTBUFFER_TOO_SMALL = -1,
+};
+
+
+
+extern "C" {
+
+// TODO: exception handling
+int skeletonize_wasm(
+    // input
+    bool*   binarymap,
+    size_t  width,
+    size_t  height,
+    // output
+    bool*   output,
+    size_t  output_size
+){
+    if(output_size < width * height)
+        return OUTPUTBUFFER_TOO_SMALL;
+
+    const EigenMapToBinaryMap x_t(binarymap, height, width);
+    EigenBinaryMap y_t = skeletonize(x_t);
+
+    // copy result to output
+    EigenMapToBinaryMap(output, height, width) = y_t;
+    
+    return OK;
+}
+
+}
