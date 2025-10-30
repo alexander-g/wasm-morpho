57950afb83884dbace0bd72a99921f2c7677fb3f
alexander-g <3867427+alexander-g@users.noreply.github.com>
2025-10-30 17:25:26 +0000

    skeletonization speedup

--- morphology.cpp
+++ morphology.cpp
@@ -5,6 +5,14 @@
 
 typedef Eigen::Array<bool,8,1> Array8b;
 
+typedef struct Index2D {
+    Eigen::Index i;
+    Eigen::Index j;
+} Index2D;
+typedef std::vector<Index2D> Indices2D;
+
+
+
 static Array8b get_neighbors_of_pixel(
     const EigenBinaryMap& x, 
     Eigen::Index i, 
@@ -24,13 +32,13 @@ static Array8b get_neighbors_of_pixel(
 }
 
 /** Compute if the number of neighbors of a pixel is >=2 and <=6 */
-static bool skeletonize_condition_a(const Array8b&  P) {
+static bool _skeletonize_condition_a(const Array8b&  P) {
     const int sum = (P.cast<int>()).sum();
     return sum >= 2 && sum <= 6;
 }
 
 /** Compute the number of 0-1 transitions in P2...P9 */
-static bool skeletonize_condition_b(const Array8b&  P) {
+static bool _skeletonize_condition_b(const Array8b&  P) {
     const int T_23 = !P(0) && P(1);
     const int T_34 = !P(1) && P(2);
     const int T_45 = !P(2) && P(3);
@@ -44,54 +52,51 @@ static bool skeletonize_condition_b(const Array8b&  P) {
 }
 
 /** Condition c): P2 * P4 * P6 = 0 */
-const bool skeletonize_condition_c0(const Array8b& P) {
+const bool _skeletonize_condition_c0(const Array8b& P) {
     return (P(0) && P(2) && P(4)) == 0;
 }
 
 /** Condition d): P4 * P6 * P8 == 0 */
-const bool skeletonize_condition_d0(const Array8b& P) {
+const bool _skeletonize_condition_d0(const Array8b& P) {
     return (P(2) && P(4) && P(6)) == 0;
 }
 
-/** Condition c): P2 * P4 * P8 = 0 */
-const bool skeletonize_condition_c1(const Array8b& P) {
+/** Condition c'): P2 * P4 * P8 = 0 */
+const bool _skeletonize_condition_c1(const Array8b& P) {
     return (P(0) && P(2) && P(6)) == 0;
 }
 
-/** Condition d): P2 * P6 * P8 == 0 */
-const bool skeletonize_condition_d1(const Array8b& P) {
+/** Condition d'): P2 * P6 * P8 == 0 */
+const bool _skeletonize_condition_d1(const Array8b& P) {
     return (P(0) && P(4) && P(6)) == 0;
 }
 
 
 
-
-
-
-static EigenBinaryMap skeletonize_subiteration(
+static EigenBinaryMap _skeletonize_subiteration(
     const EigenBinaryMap& x,
+    const Indices2D& nonzeroindices,
     bool  subiter_2
 ) {
     EigenBinaryMap mask(x.dimensions());
     mask.setZero();
 
-    for (Eigen::Index i = 1; i < x.dimension(0)-1; i++)
-        for (Eigen::Index j = 1; j < x.dimension(1)-1; j++)
-            if( x(i,j) ){
-                const Array8b P = get_neighbors_of_pixel(x, i, j);
-                const bool a = skeletonize_condition_a(P);
-                const bool b = skeletonize_condition_b(P);
-                const bool c = 
-                    !subiter_2
-                        ? skeletonize_condition_c0(P) 
-                        : skeletonize_condition_c1(P);
-                const bool d = 
-                    !subiter_2
-                        ? skeletonize_condition_d0(P) 
-                        : skeletonize_condition_d1(P);
+    for( const Index2D& index: nonzeroindices )
+        if( x(index.i, index.j) ){
+            const Array8b P = get_neighbors_of_pixel(x, index.i, index.j);
+            const bool a = _skeletonize_condition_a(P);
+            const bool b = _skeletonize_condition_b(P);
+            const bool c = 
+                !subiter_2
+                    ? _skeletonize_condition_c0(P) 
+                    : _skeletonize_condition_c1(P);
+            const bool d = 
+                !subiter_2
+                    ? _skeletonize_condition_d0(P) 
+                    : _skeletonize_condition_d1(P);
 
-                mask(i,j) = (a && b && c && d);
-            }
+            mask(index.i, index.j) = (a && b && c && d);
+        }
 
     return mask;
 }
@@ -105,24 +110,30 @@ static auto remove_padding(EigenBinaryMap& x) {
 }
 
 static EigenBinaryMap copy_and_pad(const EigenBinaryMap& x) {
-    const Eigen::array<Eigen::Index, 2> new_dims = 
-        {x.dimension(0)+2, x.dimension(1)+2};
-    EigenBinaryMap dst(new_dims);
-    dst.setZero();
-
-    // copy data into a slice that removes padding
-    remove_padding(dst) = x;
-    return dst;
+    Eigen::array<std::pair<Eigen::Index,Eigen::Index>, 2> paddings{
+        std::make_pair(1,1), 
+        std::make_pair(1,1)
+    };
+    return x.pad(paddings).eval();
 }
 
 
 /** I cannot get `bool ok = mask.any()` to work ...help. */
 bool _any(const EigenBinaryMap& mask) {
-    bool value = false;
     for (Eigen::Index i = 0; i < mask.dimension(0); i++)
         for (Eigen::Index j = 0; j < mask.dimension(1); j++)
-            value = value | mask(i,j);
-    return value;
+            if( mask(i,j) )
+                return true;
+    return false;
+}
+
+Indices2D argwhere(const EigenBinaryMap& x) {
+    Indices2D result;
+    for (Eigen::Index i = 1; i < x.dimension(0)-1; i++)
+        for (Eigen::Index j = 1; j < x.dimension(1)-1; j++)
+            if( x(i,j) )
+                result.push_back({i,j});
+    return result;
 }
 
 
@@ -131,13 +142,16 @@ EigenBinaryMap skeletonize(const EigenMapToBinaryMap input) {
     const auto dim1 = input.dimension(1);
 
     EigenBinaryMap padded = copy_and_pad(input);
+    const Indices2D nonzeroindices = argwhere(padded);
 
     bool finished = false;
     while(!finished) {
-        const EigenBinaryMap mask1 = skeletonize_subiteration(padded, false);
+        const EigenBinaryMap mask1 = 
+            _skeletonize_subiteration(padded, nonzeroindices, false);
         padded = padded * !mask1;
 
-        const EigenBinaryMap mask2 = skeletonize_subiteration(padded, true);
+        const EigenBinaryMap mask2 = 
+            _skeletonize_subiteration(padded, nonzeroindices, true);
         padded = padded * !mask2;
 
         finished = !_any(mask1) && !_any(mask2);
