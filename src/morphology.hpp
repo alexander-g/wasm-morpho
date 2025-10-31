a4e655e8e266360ebd238da754c4fea39aa25ec2
alexander-g <3867427+alexander-g@users.noreply.github.com>
2025-10-31 20:55:41 +0000

    headerfile update

--- src/morphology.hpp
+++ src/morphology.hpp
@@ -8,3 +8,32 @@ typedef Eigen::TensorMap<EigenBinaryMap> EigenMapToBinaryMap;
 /** Skeletonization as in https://doi.org/10.1145/357994.358023 */
 EigenBinaryMap skeletonize(const EigenMapToBinaryMap input);
 
+
+
+typedef struct Index2D {
+    Eigen::Index i;
+    Eigen::Index j;
+
+    bool operator==(const Index2D& other) const { 
+        return i == other.i && j == other.j; 
+    }
+} Index2D;
+typedef std::vector<Index2D> Indices2D;
+
+
+
+typedef struct DFS_Result {
+    /** Indices/pixels in order of visit  */
+    Indices2D visited;
+
+    /** Predecessor pixels along a path. Values indexing into `visited`. */
+    std::vector<int> predecessors;
+
+    /** Terminal pixels, first/last in a path. Values indexing into `visited` */
+    std::vector<int> leaves;
+} DFS_Result;
+
+
+/** Depth-first search*/
+DFS_Result dfs(const EigenBinaryMap& input, const Index2D& start);
+
